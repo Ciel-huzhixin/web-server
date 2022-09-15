c1c54d6d5615fa8a7b82e0fda7261c0774d8383b
xin <1844985716@qq.com>
2022-09-15 23:39:05 +0800

    feat: add user info

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const users = [
   { username: 'huzhixin', age: 22 },
   { username: 'huqiao', age: 23 },
   { username: 'zhangying', age: 22 },
-  { username: 'linjianan', age: 25}
+  { username: 'linjianan', age: 25},
+  { username: 'guorenhui', age: 23 }
 ];
 
 app.get('/api/users', (req, res) => {
